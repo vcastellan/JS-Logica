e5a37577c88cd6120dac0cace197d50d06aab6c5
Victor Castellan <victor_castellan@hotmail.com>
2023-10-23 12:17:51 -0300

    Recriando o Repositorio

---
+++ JS 01/numeros-secretos/app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao jogo dos Números Secretos!');
+let numeroSecreto = 5;
+let chute = prompt ('Escolha um número entre 1 e 10');
+
+if (numeroSecreto == chute) {
+    console.log('Isso aí! Você descobriu o número secreto (5)');
+}
